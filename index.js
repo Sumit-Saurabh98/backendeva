890616a503286544a8744ef2438c002cd04639e4
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-06-27 01:56:14 +0530

    sdfa

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ const generateToken = (user)=>{
 }
 
 const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'];
+    // const token = req.headers['authorization'];
+    const token = req.headers.authorization?.split(" ")[1]
     if(!token){
         return res.status(401).json({message: "No token Provided"})
     }
